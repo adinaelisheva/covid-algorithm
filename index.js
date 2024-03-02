61eddfd79cd8131bed3c2a03b8ee89d07e267656
Adina Karantza <adina.elisheva@gmail.com>
2024-03-02 22:23:07 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,108 @@
+// Worry level adjustments can be made here:
+covidNumberWorryLevels = [
+  50, // Level 0. Below this and covid is "over" - do anything
+  500, // Level 1. Below this and I can stop masking in stores etc
+  1000, // Level 2. Below this and I don't have to worry about restaurants
+  2500 // Level 3. Be more careful about events, restaurants, etc
+  // Above this = SURGE! Start staying home.
+];
+
+window.onload = () => {
+  // Gather data
+  const covidNumbers = mwradata[1];
+  const lastCovid = Date.parse(personaldata[0]);
+  const lastBooster = Date.parse(personaldata[1]);
+  const now = new Date().getTime();
+
+  const msPerDay = 1000 * 60 * 60 * 24;
+  const daysSinceBooster = (now - lastBooster) / msPerDay;
+  const daysSinceCovid = (now - lastCovid) / msPerDay;
+  
+  // Update fields
+  document.querySelector(".date").innerHTML = mwradata[0];
+  document.querySelector(".covidNums").innerHTML = covidNumbers;
+  if (covidNumbers < 1000) {
+    document.querySelector(".covidNums").classList.add("greenBg");
+  } else if(covidNumbers < 2000) {
+    document.querySelector(".covidNums").classList.add("yellowBg");
+  } else {
+    document.querySelector(".covidNums").classList.add("redBg");
+  }
+  document.querySelector(".sinceCovid").innerHTML = getFlexibleTimeString(daysSinceCovid);
+  document.querySelector(".sinceBooster").innerHTML = getFlexibleTimeString(daysSinceBooster);
+
+  let worryLevel = 0;
+  if (covidNumbers < covidNumberWorryLevels[0]) {
+    worryLevel = 0; // No worry at all
+  } else if (covidNumbers < covidNumberWorryLevels[1]) {
+    worryLevel = 1; // Low worry - No need to mask in stores etc
+  } else if (covidNumbers < covidNumberWorryLevels[2]) {
+    worryLevel = 2; // Regular worry - mask in stores but don't worry about eating out
+  } else if (covidNumbers < covidNumberWorryLevels[3]) {
+    worryLevel = 3; // Slightly extra worry - try to eat out less
+  } else {
+    worryLevel = 4; // Surge! Try to stay home
+  }
+  if (daysSinceCovid  < 30 || daysSinceBooster < 30) {
+    worryLevel = 0;
+  }
+
+  const eventMask = document.querySelector(".eventMask");
+  const tMask = document.querySelector(".tMask");
+  const storeMask = document.querySelector(".storeMask");
+  const dining = document.querySelector(".dining");
+  const wfh = document.querySelector(".wfh");
+
+  // Mask on the T and at events basically always
+  if (worryLevel < 1) {
+    eventMask.innerHTML = "N";
+    eventMask.classList.add("green");
+    tMask.innerHTML = "N";
+    tMask.classList.add("green");
+  } else {
+    eventMask.innerHTML = "Y";
+    eventMask.classList.add("red");
+    tMask.innerHTML = "Y";
+    tMask.classList.add("red");
+  }
+
+  if (worryLevel < 2) {
+    storeMask.innerHTML = "N";
+    storeMask.classList.add("green");
+  } else {
+    storeMask.innerHTML = "Y";
+    storeMask.classList.add("red");
+  }
+
+  if (worryLevel < 3) {
+    dining.innerHTML = "Y";
+    dining.classList.add("green");
+  } else if (worryLevel < 4) {
+    dining.innerHTML = "rarely";
+    dining.classList.add("yellow");
+  } else {
+    dining.innerHTML = "N";
+    dining.classList.add("red");
+  }
+
+  if (worryLevel < 4) {
+    wfh.innerHTML = "N";
+    wfh.classList.add("green");
+  } else {
+    wfh.innerHTML = "Y";
+    wfh.classList.add("red");
+  }
+
+}
+
+function getFlexibleTimeString(days) {
+  if (days < 14) {
+    return days + " days";
+  }
+  weeks = Math.floor(days / 7);
+  if (weeks < 9) {
+    return weeks + " weeks";
+  }
+  months = Math.floor(days / 30);
+  return months + " months";
+}
---
+++ personal.js
@@ -0,0 +1,4 @@
+personaldata = [
+  "10/15/2023", // Last booster
+  "9/1/2023", // Last COVID case
+];
