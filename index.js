28ceb791c6d4d2afc08c4843ad36fbe156446101
Adina Karantza <adina.elisheva@gmail.com>
2024-03-03 21:21:43 +0000

    color recency numbers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // --- Worry level adjustments can be made here: --------------------
 covidNumberWorryLevels = [
-  50, // Level 0. Below this and covid is "over" - do anything
+  50, // Level 0. Below this and covid is 'over' - do anything
   500, // Level 1. Below this and I can stop masking in stores etc
   1000, // Level 2. Below this and I don't have to worry about restaurants
   2500 // Level 3. Be more careful about events, restaurants, etc
@@ -26,17 +26,17 @@ window.onload = () => {
   const daysSinceCovid = (now - lastCovid) / msPerDay;
   
   // Update fields
-  document.querySelector(".date").innerHTML = mwradata[0];
-  document.querySelector(".covidNums").innerHTML = covidNumbers;
+  document.querySelector('.date').innerHTML = mwradata[0];
+  document.querySelector('.covidNums').innerHTML = covidNumbers;
   if (covidNumbers < 1000) {
-    document.querySelector(".covidNums").classList.add("greenBg");
+    document.querySelector('.covidNums').classList.add('greenBg');
   } else if(covidNumbers < 2000) {
-    document.querySelector(".covidNums").classList.add("yellowBg");
+    document.querySelector('.covidNums').classList.add('yellowBg');
   } else {
-    document.querySelector(".covidNums").classList.add("redBg");
+    document.querySelector('.covidNums').classList.add('redBg');
   }
-  document.querySelector(".sinceCovid").innerHTML = getFlexibleTimeString(daysSinceCovid);
-  document.querySelector(".sinceBooster").innerHTML = getFlexibleTimeString(daysSinceBooster);
+  document.querySelector('.sinceCovid').innerHTML = getFlexibleTimeString(daysSinceCovid);
+  document.querySelector('.sinceBooster').innerHTML = getFlexibleTimeString(daysSinceBooster);
 
   let worryLevel = 0;
   if (covidNumbers < covidNumberWorryLevels[0]) {
@@ -50,69 +50,80 @@ window.onload = () => {
   } else {
     worryLevel = 4; // Surge! Try to stay home
   }
-  if (daysSinceCovid  < covidRecentCutoff || daysSinceBooster < covidRecentCutoff) {
+  if (daysSinceCovid  < covidRecentCutoff) {
     worryLevel = 0;
+    document.querySelector('.sinceCovid').classList.add('green');
+  }
+  if (daysSinceBooster < covidRecentCutoff) {
+    worryLevel = 0;
+    document.querySelector('.sinceBooster').classList.add('green');
   }
   if (daysSinceCovid > covidDistantCutoff && daysSinceBooster > covidDistantCutoff && worryLevel < 4) {
     worryLevel++;
   }
+  if (daysSinceCovid  > covidDistantCutoff) {
+    document.querySelector('.sinceCovid').classList.add('red');
+  }
+  if (daysSinceBooster > covidDistantCutoff) {
+    document.querySelector('.sinceBooster').classList.add('red');
+  }
 
-  const eventMask = document.querySelector(".eventMask");
-  const tMask = document.querySelector(".tMask");
-  const storeMask = document.querySelector(".storeMask");
-  const dining = document.querySelector(".dining");
-  const wfh = document.querySelector(".wfh");
+  const eventMask = document.querySelector('.eventMask');
+  const tMask = document.querySelector('.tMask');
+  const storeMask = document.querySelector('.storeMask');
+  const dining = document.querySelector('.dining');
+  const wfh = document.querySelector('.wfh');
 
   // Mask on the T and at events basically always
   if (worryLevel < 1) {
-    eventMask.innerHTML = "N";
-    eventMask.classList.add("green");
-    tMask.innerHTML = "N";
-    tMask.classList.add("green");
+    eventMask.innerHTML = 'N';
+    eventMask.classList.add('green');
+    tMask.innerHTML = 'N';
+    tMask.classList.add('green');
   } else {
-    eventMask.innerHTML = "Y";
-    eventMask.classList.add("red");
-    tMask.innerHTML = "Y";
-    tMask.classList.add("red");
+    eventMask.innerHTML = 'Y';
+    eventMask.classList.add('red');
+    tMask.innerHTML = 'Y';
+    tMask.classList.add('red');
   }
 
   if (worryLevel < 2) {
-    storeMask.innerHTML = "N";
-    storeMask.classList.add("green");
+    storeMask.innerHTML = 'N';
+    storeMask.classList.add('green');
   } else {
-    storeMask.innerHTML = "Y";
-    storeMask.classList.add("red");
+    storeMask.innerHTML = 'Y';
+    storeMask.classList.add('red');
   }
 
   if (worryLevel < 3) {
-    dining.innerHTML = "Y";
-    dining.classList.add("green");
+    dining.innerHTML = 'Y';
+    dining.classList.add('green');
   } else if (worryLevel < 4) {
-    dining.innerHTML = "rarely";
-    dining.classList.add("yellow");
+    dining.innerHTML = 'rarely';
+    dining.classList.add('yellow');
   } else {
-    dining.innerHTML = "N";
-    dining.classList.add("red");
+    dining.innerHTML = 'N';
+    dining.classList.add('red');
   }
 
   if (worryLevel < 4) {
-    wfh.innerHTML = "N";
-    wfh.classList.add("green");
+    wfh.innerHTML = 'N';
+    wfh.classList.add('green');
   } else {
-    wfh.innerHTML = "Y";
-    wfh.classList.add("red");
+    wfh.innerHTML = 'Y';
+    wfh.classList.add('red');
   }
 
 }
 
 function getFlexibleTimeString(days) {
   if (days < 14) {
-    return days + " days";
+    return days + ' days';
   }
   weeks = Math.floor(days / 7);
   if (weeks < 9) {
-    return weeks + " weeks";
+    return weeks + ' weeks';
   }
   months = Math.floor(days / 30);
-  return months + " months";
+  return months + ' months';
 }
